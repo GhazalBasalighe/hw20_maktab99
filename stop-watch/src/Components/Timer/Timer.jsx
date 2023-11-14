bbe6bbf618eeaf3b1186f63bd213b61d426776a8
Ghazal Basalighe <ghazalbasaligheh@gmail.com>
2023-11-14 17:28:03 +0330

    first commit

---
+++ stop-watch/src/Components/Timer/Timer.jsx
@@ -0,0 +1,12 @@
+function Timer() {
+  return (
+    <div>
+      <p>33:12:00</p>
+      <button className="btn btn-start">Start</button>
+      <button className="btn btn-lap">Lap</button>
+      <button className="btn btn-stop">Stop</button>
+    </div>
+  );
+}
+
+export default Timer;
